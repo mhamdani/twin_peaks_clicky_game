b0f3ad0ec699d4ac13df196e6711475a63d0fce1
Michelle Hamdani <michellehamdani@gmail.com>
2018-01-08 14:49:29 -0800

    add backdrop

--- src/components/Jumbo/Jumbo.js
+++ src/components/Jumbo/Jumbo.js
@@ -4,7 +4,7 @@ import './Jumbo.css';
 const Jumbo = props => (
 
   <div className="jumbotron">
-    <h1 className="display-3 text-center">Ready to play?</h1>
+    <h1 className="display-3">Ready to play?</h1>
     <h3 className="text-center">Try to figure out the logic of the game!<span id="hint"> Hint: pay attention to your score!</span></h3>
   </div>
 
