1b425db1ff6fd735fb9faa3ff92ffb504c36f6d9
Michelle Hamdani <michellehamdani@gmail.com>
2018-01-08 20:53:08 -0800

    final spec update

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,8 +4,8 @@ import './Header.css';
 const Header = props => (
 
   <div>
-    <div className="navbar navbar-expand-lg navbar-light bg-dark fixed-top" >
-      <h2 className="navbar brand text-light"><span id="twin_peaks">Twin Peaks</span> <span id="clicky">Clicky Game</span></h2>
+      <div className="navbar navbar-expand-lg navbar-light fixed-top parallax">
+      <h2 className="brand text-light"><span id="twin_peaks">Twin Peaks</span> <span id="clicky">Clicky Game</span></h2>
       <h3 className="nav-link text-light text-right">Your Score: <span>{props.currentScore}</span></h3>
       <h3 className="nav-link text-light text-right">High Score: <span>{props.highScore}</span></h3>
     </div>
--- src/components/Jumbo/Jumbo.js
+++ src/components/Jumbo/Jumbo.js
@@ -3,8 +3,10 @@ import './Jumbo.css';
 
 const Jumbo = props => (
 
-  <div className="jumbotron">
+  <div className="jumbotron parallax">
+  <div className="jumbotron top">
     <h1 className="display-3">Ready to play?</h1>
+  </div>
     <h3 className="text-center">Try to figure out the logic of the game!<span id="hint"> Hint: pay attention to your score!</span></h3>
   </div>
 
