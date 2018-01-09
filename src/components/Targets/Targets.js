11f6ab1c2a3a3b07757355e75abe41cf56708143
Michelle Hamdani <michellehamdani@gmail.com>
2018-01-08 18:30:48 -0800

    revert

--- src/components/Targets/Targets.js
+++ src/components/Targets/Targets.js
@@ -8,9 +8,7 @@ const Targets = props => (
   <div onClick={() => props.alreadySelected(props.id)} className="card">
     <div className="img-container target">
       <Overdrive id={props.id}>
-      <div role="img"
-        aria-label="click item"
-        style={{ backgroundImage: 'url("${props.image}")' }} />
+        <img alt={props.name} src={props.image} />
       </Overdrive>
     </div>
   </div>
